eb0d64b3bf69bfe6e81cc8075561e7a3fd3b5ac9
Vstick <guillaumemara83@gmail.com>
2025-03-18 21:56:00 +0100

    je fais le LCD youpi youpi yaaa

---
+++ src/LCD.hpp
@@ -0,0 +1,21 @@
+#ifndef LCD_HPP
+#define LCD_HPP
+
+class LCD
+{
+private:
+    /* data */
+public:
+    LCD(/* args */);
+    ~LCD();
+};
+
+LCD::LCD(/* args */)
+{
+}
+
+LCD::~LCD()
+{
+}
+
+#endif
