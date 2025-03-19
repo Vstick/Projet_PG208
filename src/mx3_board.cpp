c365dc9b020e0446d8c244cce874aa05a33ba9f4
Vstick <guillaumemara83@gmail.com>
2025-03-19 11:05:47 +0100

    Ajout duLCD qui compile

--- src/mx3_board.cpp
+++ src/mx3_board.cpp
@@ -1,5 +1,9 @@
 #include "mx3_board.hpp"
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// MX3 board //////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 MX3board::MX3board(const char* filename) {
     board_open(filename);
 }
@@ -12,7 +16,24 @@ Reg8 MX3board::operator[](unsigned char addr){
     return Reg8(this, addr);
 }
 
+void MX3board::writeLCD(unsigned char addr, char value) {
+    (*this)[addr] = value;  // Accède directement à l'adresse et écrit dans le registre
+}
 
+MX3board& MX3board::operator<<(const std::string& message) {
+    lcd->write(message);  // Appel de la méthode write sur LCD pour écrire le message
+    lcd->display(*this);       // Affichage du texte à l'écran
+    return *this;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// Reg8 ///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Reg8::Reg8(MX3board* card, unsigned char add){
+    this->board = card;
+    this->addr = add;
+}
 
 void Reg8::operator=(unsigned char value){
     board_d_write(board->tty, addr, value);
@@ -20,4 +41,46 @@ void Reg8::operator=(unsigned char value){
 
 Reg8::operator unsigned char() const{
     return board_d_read(board->tty, addr);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// LCD ////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LCD::LCD(){
+    // Initialisation des lignes LCD à vide
+    std::fill(std::begin(line1), std::end(line1), ' ');
+    std::fill(std::begin(line2), std::end(line2), ' ');
+}
+
+void LCD::write(const std::string& message){
+    static int pos = 0;
+    for (char c : message) {
+        if (pos < 16) {
+            line2[pos] = c;
+        } else {
+            // Quand la ligne 2 est pleine, on déplace la ligne 2 dans la ligne 1
+            for (int i = 0; i < 16; i++) {
+                line1[i] = line2[i];
+                line2[i] = ' ';
+            }
+            line2[0] = c; // Remise du caractère dans la ligne 2
+            pos = 0;
+        }
+        pos++;
+    }
+}
+
+void LCD::next(){
+    for (int i = 0; i < 16; i++) {
+        line1[i] = line2[i];
+    }
+    std::fill(std::begin(line2), std::end(line2), ' ');
+}
+
+void LCD::display(MX3board& board){
+    for (int i = 0; i < 16; i++) {
+        board.writeLCD(MX3ADDR_LCD_START + i, line1[i]);
+        board.writeLCD(MX3ADDR_LCD_START + 16 + i, line2[i]);
+    }
 }
--- src/mx3_board.hpp
+++ src/mx3_board.hpp
@@ -4,36 +4,55 @@
 #include "mx3board_regs.h"
 #include "common_mx3.hpp"
 #include <stdexcept>
+#include <sstream>
+#include <string>
 
 class Reg8;
+class LCD;
 
 class MX3board {
 private:
-    int tty;
+    friend class Reg8;
+    friend class LCD;
 
+    int tty;
+    LCD* lcd;
 public:
     MX3board(const char* filename);
     ~MX3board();
 
     Reg8 operator[](unsigned char addr);
 
-    friend class Reg8;
+    // Méthode pour manipuler les registres LCD
+    void writeLCD(unsigned char addr, char value);
+    // Surcharge de l'opérateur << pour écrire sur l'écran LCD via la classe LCD
+    MX3board& operator<<(const std::string& message);
 };
 
 class Reg8 {
-    private:
-        MX3board* board;
-        unsigned char addr;
-    
-    public:
-        Reg8(MX3board* card, unsigned char add){
-            this->board = card;
-            this->addr = add;
-        }
-    
-        void operator=(unsigned char value);
-        operator unsigned char() const;
-    };
-    
+private:
+    MX3board* board;
+    unsigned char addr;
+
+public:
+    Reg8(MX3board* card, unsigned char add);
+    void operator=(unsigned char value);
+    operator unsigned char() const;
+};
+
+class LCD 
+{
+private:
+    char line1[16];
+    char line2[16];
+
+public:
+    LCD();
+    void write(const std::string& message);
+    void next();
+
+    // Affichage du message sur le LCD via les registres
+    void display(MX3board& board);
+};
 
 #endif // MX3BOARD_HPP
